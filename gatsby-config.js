aec3ad44c315803fc4a7264f93e01c5988def879
Josu <56826352+josugoar@users.noreply.github.com>
2021-03-08 17:38:32 +0000

    Add Gatsby configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,23 @@
-const fs = require("fs")
-const fetch = require("node-fetch")
-const { buildClientSchema } = require("graphql")
-const { createHttpLink } = require("apollo-link-http")
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 module.exports = {
   siteMetadata: {
-    title: "josugoar.github.io",
+    siteUrl: "https://josugoar.github.io",
+    title: "josugoar (Josu)",
   },
   plugins: [
+    "gatsby-plugin-sass",
     {
       resolve: "gatsby-source-graphql",
       options: {
-        createLink: () => {
-          return createHttpLink({
-            uri: "https://api.github.com/graphql",
-            headers: {
-              Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
-            },
-            fetch,
-          })
-        },
-        createSchema: async () => {
-          const json = JSON.parse(fs.readFileSync(`${__dirname}/github.json`))
-          return buildClientSchema(json.data)
-        },
-        fieldName: "github",
         typeName: "GitHub",
+        fieldName: "github",
+        url: "https://api.github.com/graphql",
+        headers: {
+          Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+        },
       },
     },
   ],
