85e824d9e8294637876d50c947b518d77a5a3a96
Josu <56826352+josugoar@users.noreply.github.com>
2021-03-08 17:40:55 +0000

    Migrate repository to primer

--- src/components/repository.tsx
+++ src/components/repository.tsx
@@ -1,31 +1,15 @@
-import {
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Chip,
-  Link,
-  makeStyles,
-  Typography,
-} from "@material-ui/core"
+import { graphql } from "gatsby"
 import React from "react"
-import RepositoryTopicConnection, {
-  RepositoryTopicConnectionProps,
-} from "./repositoryTopicConnection"
+import { LanguageConnectionProps } from "./languageConnection"
+import RepositoryMedia from "./repositoryMedia"
+import RepositoryContent from "./repositoryContent"
+import RepositoryHeader from "./repositoryHeader"
+import RepositoryNavigation from "./repositoryNavigation"
+import { RepositoryTopicConnectionProps } from "./repositoryTopicConnection"
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-})
-
-type RepositoryProps = {
+export type RepositoryProps = {
   description: string | null
-  id: string
+  languages: LanguageConnectionProps
   name: string
   openGraphImageUrl: string
   repositoryTopics: RepositoryTopicConnectionProps
@@ -34,69 +18,40 @@ type RepositoryProps = {
 }
 
 const Repository = (props: RepositoryProps) => {
-  const classes = useStyles()
-
   return (
-    <Link
-      className={classes.root}
-      href={props.url}
-      rel="noopener noreferrer"
-      target="_blank"
-      underline="none"
-    >
-      <Card>
-        <CardActionArea>
-          {props.usesCustomOpenGraphImage && (
-            <CardMedia
-              alt={props.name}
-              className={classes.media}
-              component="img"
-              height="140"
-              image={props.openGraphImageUrl}
-              title={props.name}
-            />
-          )}
-          <CardContent>
-            <Typography component="h2" gutterBottom variant="h5">
-              {props.name}
-            </Typography>
-            <Typography color="textSecondary" component="p" variant="body2">
-              {props.description}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <RepositoryTopicConnection nodes={props.repositoryTopics.nodes} />
-          </CardActions>
-        </CardActionArea>
-      </Card>
-    </Link>
+    <article className="border rounded-1 box-shadow bg-gray-light my-4">
+      {props.usesCustomOpenGraphImage && (
+        <RepositoryMedia
+          name={props.name}
+          openGraphImageUrl={props.openGraphImageUrl}
+          url={props.url}
+        />
+      )}
+      <RepositoryHeader name={props.name} url={props.url} />
+      <RepositoryNavigation url={props.url} />
+      <RepositoryContent
+        description={props.description}
+        languages={props.languages}
+        repositoryTopics={props.repositoryTopics}
+      />
+    </article>
   )
 }
 
 export default Repository
 
-// https://material-ui.com/components/pagination/
-// https://material-ui.com/components/skeleton/
-// https://material-ui.com/customization/theming/
-// https://material-ui.com/components/transitions/
-
-// export const query = graphql`
-//   fragment RepositoryFragment {
-//     repository(name: $name, owner: $owner) {
-//       description
-//       id
-//       name
-//       openGraphImageUrl
-//       repositoryTopics(first: 10) {
-//         nodes {
-//           id
-//           topic {
-//             name
-//           }
-//         }
-//       }
-//       url
-//       usesCustomOpenGraphImage
-//     }
-//   }
-// `
+export const query = graphql`
+  fragment RepositoryFragment on GitHub_Repository {
+    description
+    languages(first: 10) {
+      ...LanguageConnectionFragment
+    }
+    name
+    openGraphImageUrl
+    repositoryTopics(first: 20) {
+      ...RepositoryTopicConnectionFragment
+    }
+    url
+    usesCustomOpenGraphImage
+  }
+`
