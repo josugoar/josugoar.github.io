e6801ba46ee854e42f3e1214930009513849abd2
Josu <56826352+josugoar@users.noreply.github.com>
2021-03-08 17:40:41 +0000

    Add pinableItemConnectionHeader

---
+++ src/components/pinnableItemConnectionHeader.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const PinnableItemConnectionHeader = () => {
+  return <h2 className="h3-mktg text-gray mb-n2">Featured repositories...</h2>
+}
+
+export default PinnableItemConnectionHeader
