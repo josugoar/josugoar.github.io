7dadb67a01e0035f65a53fb4e490ef569b848892
Josu <56826352+josugoar@users.noreply.github.com>
2021-03-08 17:42:44 +0000

    Migrate index to primer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,57 @@
-import { makeStyles } from "@material-ui/core"
+import { graphql } from "gatsby"
 import React from "react"
-import Repository from "../components/repository"
-import User from "../components/user"
+import PinnableItemConnection, {
+  PinnableItemConnectionProps,
+} from "../components/pinnableItemConnection"
+import User, { UserProps } from "../components/user"
 
-const useStyles = makeStyles({
-  root: {
-    color: "#232129",
-    padding: "96px",
-    fontFamily: "-apple-system, Roboto, sans-serif, serif",
-  },
-})
+type IndexPageProps = {
+  data: {
+    github: {
+      viewer: UserProps
+      pinnedItems: PinnableItemConnectionProps
+    }
+  }
+}
 
-const IndexPage = () => {
-  const classes = useStyles()
-
+const IndexPage = (props: IndexPageProps) => {
   return (
-    <main className={classes.root}>
-      <title>Home Page</title>
-      <User
-        avatarUrl="https://avatars.githubusercontent.com/u/56826352?u=d11278eb01ab269b53c1d874f289cefcfa2b3980&v=4"
-        bio="Interested in machine learning, system administration and web development"
-        login="josugoar"
-        name="Josu"
-        url="https://github.com/josugoar"
-      />
-      {/* <Repository
-        description="Unopinionated argparse wrapper"
-        id="0"
-        name="argdeco"
-        openGraphImageUrl="https://repository-images.githubusercontent.com/287952169/26085280-6a11-11eb-860d-92676f499bb6"
-        repositoryTopics={{
-          nodes: [
-            {
-              id: "0",
-              topic: {
-                name: "argparse",
-              },
-            },
-            {
-              id: "1",
-              topic: {
-                name: "wrapt",
-              },
-            },
-            {
-              id: "2",
-              topic: {
-                name: "python",
-              },
-            },
-            {
-              id: "3",
-              topic: {
-                name: "pip",
-              },
-            },
-          ],
-        }}
-        url="https://github.com/josugoar/argdeco"
-        usesCustomOpenGraphImage={true}
-      /> */}
+    <main>
+      <div
+        className="border-bottom border-gray-light"
+        style={{ backgroundColor: "#fcfdfd" }}
+      >
+        <div className="container-xl p-responsive">
+          <div className="d-md-flex flex-wrap flex-lg-nowrap gutter-md">
+            <User
+              avatarUrl={props.data.github.viewer.avatarUrl}
+              bio={props.data.github.viewer.bio}
+              login={props.data.github.viewer.login}
+              name={props.data.github.viewer.name}
+              pinnedItems={props.data.github.viewer.pinnedItems}
+              url={props.data.github.viewer.url}
+            />
+            <PinnableItemConnection
+              nodes={props.data.github.viewer.pinnedItems.nodes}
+            />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
 
 export default IndexPage
 
-// export const query = graphql`
-//   query IndexPageQuery {
-//   }
-// `
+export const query = graphql`
+  query IndexPageQuery {
+    github {
+      viewer {
+        ...UserFragment
+        pinnedItems(first: 6) {
+          ...PinnableItemConnectionFragment
+        }
+      }
+    }
+  }
+`
