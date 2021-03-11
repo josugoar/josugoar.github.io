cb5179d3c1a350e02a4925b65895b96114e12dc1
Josu <56826352+josugoar@users.noreply.github.com>
2021-03-11 17:19:31 +0000

    Remove extra pinnableItemConnection components

--- src/components/pinnableItemConnectionHeader.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const PinnableItemConnectionHeader = () => {
-  return <h2 className="h3-mktg text-gray mb-n2">Featured repositories...</h2>
-}
-
-export default PinnableItemConnectionHeader
