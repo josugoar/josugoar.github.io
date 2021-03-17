ffe8fb127a54831b295bc77ccb9f42ccee3b1e19
Josu <56826352+josugoar@users.noreply.github.com>
2021-03-17 18:00:37 +0000

    Updates

--- sw.js
+++ sw.js
@@ -115,7 +115,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-688615aeeea5f8435dc5.js`))) {
+  if (!resources || !(await caches.match(`/app-fe2b75c11f8b54f9ee42.js`))) {
     return await fetch(event.request)
   }
 
