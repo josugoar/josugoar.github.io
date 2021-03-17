279da7030ba672df05c433c1e525390aeda2b22f
Josu <56826352+josugoar@users.noreply.github.com>
2021-03-17 19:05:14 +0000

    Redesign 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,32 @@
+import { Link } from "gatsby"
 import React from "react"
 
 const NotFoundPage = () => (
-  <main>
+  <main className="font-mktg">
     <title>Page not found</title>
     <div
-      style={{
-        alignItems: "center",
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-        justifyContent: "center",
-      }}
+      className="position-relative"
+      style={{ zIndex: 0, transition: "all 0.25s easi-in" }}
     >
-      <h1 style={{ fontSize: 128 }}>404</h1>
-      <p style={{ fontSize: 16 }}>Page not found</p>
+      <div
+        className="position-relative d-flex my-0 mx-auto overflow-hidden"
+        style={{
+          width: 940,
+          height: "100vh",
+          clear: "both",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={{ fontSize: 125 }}>404</h1>
+        <p style={{ width: 215, fontSize: 30, fontWeight: "bold" }}>
+          This is not the web page you are looking for.
+        </p>
+        <Link style={{ marginTop: 20, fontSize: 20 }} to="/">
+          Go home
+        </Link>
+      </div>
     </div>
   </main>
 )
