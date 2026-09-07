e10e6947b05524e94bc112f92d562e2eb4547c8b
Josu <56826352+josugoar@users.noreply.github.com>
2026-09-07 20:58:05 +0200

    Fix collision with Query field in schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,11 @@ const { config } = require("dotenv")
 
 config()
 
+const {
+  FilterObjectFields,
+  FilterTypes,
+} = require("@graphql-tools/wrap")
+
 module.exports = {
   plugins: [
     {
@@ -12,6 +17,27 @@ module.exports = {
         url: "https://api.github.com/graphql",
         headers: {
           Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        },
+        transformSchema: ({ schema, link, resolver, defaultTransforms }) => {
+          const { wrapSchema } = require("@graphql-tools/wrap")
+          const { linkToExecutor } = require("@graphql-tools/links")
+          return wrapSchema({
+            schema,
+            executor: linkToExecutor(link),
+            transforms: [
+              ...defaultTransforms,
+              new FilterObjectFields(
+                (typeName, fieldName) =>
+                  !(
+                    (typeName === "GitHub" && fieldName === "relay") ||
+                    (typeName === "GitHub" && fieldName === "_entities")
+                  )
+              ),
+              new FilterTypes(
+                (type) => type.name !== "GitHub__Entity"
+              ),
+            ],
+          })
         },
       },
     },
